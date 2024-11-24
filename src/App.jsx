b2c40b1a022640997f68c026626905d9038311ea
Muhendis Bey <enginierium@gmail.com>
2024-11-24 16:42:00 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <form action="">
+        <input type="text" placeholder='Name' />
+        <input type="password" placeholder='Password' />
+        <input type="submit" value="Submit" />
+      </form> 
+    </>
+  )
+}
+
+export default App
---
+++ src/components/registerationForm/registerationForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const registerationForm = () => {
+  return (
+    <div>
+        registerationForm
+    </div>
+  )
+}
